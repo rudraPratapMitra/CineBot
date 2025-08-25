2ca0dac7275d62722ee5ee893a3ff4b6fc8a9246
Rudy <rudrapratapmitra@gmail.com>
2025-08-26 03:48:53 +0530

    changed firebase_configs

--- frontend/src/Utils/firebase.js
+++ frontend/src/Utils/firebase.js
@@ -8,10 +8,10 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:  import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey:  "AIzaSyAK5gPZghzH4gQATipBN7BMhl0OANfjyVA",
+  authDomain: "cinebot-6a2ee.firebaseapp.com",
   projectId: "cinebot-6a2ee",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "cinebot-6a2ee.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
